72f6a2b45df868686bf81b8e1b86e24272897eca
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-05-19 19:23:17 -0400

    add authentication route

--- src/app.js
+++ src/app.js
@@ -5,10 +5,14 @@ const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
 
+const passport = require('passport');
+
 // version and author from our package.json file
 //const { version, author } = require('../package.json');
 
 const logger = require('./logger');
+const authorization = require('./authorization');
+
 const pino = require('pino-http')({
   // Use our default logger instance, which is already configured
   logger,
@@ -16,6 +20,13 @@ const pino = require('pino-http')({
 
 // Create an express app instance we can use to attach middleware and HTTP routes
 const app = express();
+
+// Use gzip/deflate compression middleware
+app.use(compression());
+
+// Set up our passport authorization middleware
+passport.use(authorization.strategy());
+app.use(passport.initialize());
 
 // Use logging middleware
 app.use(pino);
