c6bc59bed99641c3a0adc6f3d57e124008f4d13c
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-06-11 13:18:45 -0400

    Return data in GET route

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,4 +1,5 @@
 // src/routes/api/get.js
+const { Fragment } = require('../../model/fragment');
 
 //Refactor to Use the response.js Functions
 const { createSuccessResponse } = require('../../response');
@@ -6,11 +7,19 @@ const { createSuccessResponse } = require('../../response');
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   // TODO: this is just a placeholder to get something working...
+
+  let expand = req.query.expand;
+  let isExpand = false;
+
+  if (expand === '1') {
+    isExpand = true;
+  }
+
   res.status(200).json(
     createSuccessResponse({
-      fragments: [],
+      fragments: await Fragment.byUser(req.user, isExpand),
     })
   );
 };
--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,15 +1,15 @@
 const { Fragment } = require('../../model/fragment');
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   if (!req.is('text/plain')) {
     res.status(415).json(createErrorResponse(415, 'Content type not supported'));
     res.redirect('/');
   }
 
   const fragment = new Fragment({ ownerId: req.user, type: 'text/plain', size: 0 });
-  fragment.save();
-  fragment.setData(req.body);
+  await fragment.save();
+  await fragment.setData(req.body);
 
   res.set('Location', 'http://localhost:8080/v1/fragments/' + fragment.id);
 
