3bb6fe50f7b63e39ebeb27459a50eff9e932952f
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-06-11 12:03:44 -0400

    Add test for POST route

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -3,4 +3,29 @@ const app = require('../../src/app');
 
 describe('POST v1/fragments', () => {
   test('Unauthenticated requests are denied', () => request(app).post('/v1/fragments').expect(401));
+
+  test('Authenticated user can create a fragment', async () => {
+    const hashedEmail = '11d4c22e42c8f61feaba154683dea407b101cfd90987dda9e342843263ca420a';
+
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-type', 'text/plain')
+      .send('Hello-World');
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body.status).toBe('ok');
+    expect(res.body.fragment.ownerId).toEqual(hashedEmail);
+    expect(res.body.fragment.size).toEqual(11);
+  });
+
+  test('Authenticated user create unsupported content type', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-type', 'application/json')
+      .send({ Hello: 'World' });
+
+    expect(res.statusCode).toBe(415);
+  });
 });
